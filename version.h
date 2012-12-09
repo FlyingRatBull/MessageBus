85b71d4750f669ebe6118eaf522c657a75f5dded
Oliver Becker <der.ole.becker@gmail.com>
2012-12-09 14:18:14 +0100

    Updated version files

--- version.h
+++ version.h
@@ -1,6 +1,6 @@
-#define VERSION "0.5.0"
+#define VERSION "0.5.1"
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 5
-#define VERSION_PATCH 0
-#define VERSION_EXTRA ""
+#define VERSION_PATCH 1
+#define VERSION_EXTRA 0
 
