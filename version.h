abd02f0ac0343add66af856e39084ec556ac99a4
Oliver Becker <der.ole.becker@gmail.com>
2013-05-30 22:11:09 +0200

    Tagged version 0.6.0

--- version.h
+++ version.h
@@ -1,5 +1,5 @@
-#define VERSION "0.5.25"
+#define VERSION "0.6.0"
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 5
-#define VERSION_PATCH 25
+#define VERSION_MINOR 6
+#define VERSION_PATCH 0
 #define VERSION_EXTRA 0
