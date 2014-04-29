19618b3a13372f4fa6cf5a677563dc3cbb333fa3
Oliver Becker <der.ole.becker@gmail.com>
2014-04-29 15:25:53 +0200

    Tagged version 0.7.0

--- version.h
+++ version.h
@@ -1,5 +1,5 @@
-#define VERSION "0.6.12"
+#define VERSION "0.7.0"
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 6
-#define VERSION_PATCH 12
+#define VERSION_MINOR 7
+#define VERSION_PATCH 0
 #define VERSION_EXTRA 0
